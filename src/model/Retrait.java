198de9654074340b7de94fb178f50ba7b9d485c0
Adellatif Hissoune <haissouneabdellatif749@gmail.com>
2025-09-17 15:53:59 +0100

    here i adeed some issus

---
+++ src/model/Retrait.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Retrait extends operation{
+
+    public Retrait(double montant, String dateOperation){
+        super(montant, dateOperation);
+    }
+}
--- src/model/operation.java
+++ src/model/operation.java
@@ -1,5 +1,13 @@
 package model;
 
 public class operation {
+    protected static int numberOfOperation =0;
+    protected double montant;
+    protected String dateOperation;
 
+    public operation(double montant, String dateOperation){
+        this.montant = montant;
+        this.dateOperation = dateOperation;
+        numberOfOperation++;
+    }
 }
